e67f182f50b9fbeb7a4081f13b50a237dc91b4ab
mbahgembrong.sos@gmail.com <mbah gembrong>
2023-08-04 09:24:58 +0700

    initial commit

---
+++ app/src/main/java/com/pitjarus/pitjarusandroidtest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pitjarus.pitjarusandroidtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
